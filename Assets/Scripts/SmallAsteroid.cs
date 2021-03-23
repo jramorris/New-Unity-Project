aff76fd31af55020b780ed2d994220c0ee11dd5c
James Morris <jamesramorris@gmail.com>
2021-03-22 22:53:53 -0700

    break up asteroidd

---
+++ Assets/Scripts/SmallAsteroid.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SmallAsteroid : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // this class needs to be abstract?  Does that break pooling?
+        // either way, this is duplicated in red enemy
+        //if (collision.collider.CompareTag("Player"))
+        //    collision.collider.GetComponent<PlayerController>().TakeDamage();
+    }
+}
