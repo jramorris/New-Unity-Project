6a5c6e9a6479647de1e92c6da0e375261658068c
James Morris <jamesramorris@gmail.com>
2021-04-09 16:32:11 -0700

    indicator wip

---
+++ Assets/Scripts/ManageIndicators.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ManageIndicators : MonoBehaviour
+{
+    [SerializeField] UnityEngine.Camera mainCamera;
+    [SerializeField] Transform _playerTransform;
+
+    void LateUpdate()
+    {
+        var asteroids = GameObject.FindGameObjectsWithTag("Asteroid");
+        Debug.Log(asteroids.Length);
+        foreach(GameObject asteroid in asteroids)
+        {
+            //Vector3 screenPosition = mainCamera.WorldToViewportPoint(transform.position);
+
+            Debug.DrawRay(_playerTransform.position, asteroid.transform.position - _playerTransform.position, Color.green);
+            //Debug.Log($"Screen dims: {Screen.width} x {Screen.height}");
+            //Debug.Log($"Cam dims: {mainCamera.pixelWidth} x {mainCamera.pixelHeight}");
+            //Debug.Log($"Obj Screen Pos: {screenPosition}");
+            //if (screenPosition.x > mainCamera.pixelWidth || screenPosition.x < 0 ||
+            //    screenPosition.y > mainCamera.pixelHeight || screenPosition.y < 0)
+            //{
+            //    Debug.Log($"screen pos: {screenPosition}");
+            //}
+        }
+    }
+}
