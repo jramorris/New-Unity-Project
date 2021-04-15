4965562bc19eb335243c5972f96fd752157f09da
James Morris <jamesramorris@gmail.com>
2021-04-14 20:42:57 -0700

    count in to level

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour
+{
+    TextMeshProUGUI _tmp;
+
+    void Start()
+    {
+        Time.timeScale = 0;
+        _tmp = GetComponent<TextMeshProUGUI>();
+        StartCoroutine(StartGame(3));
+    }
+
+    IEnumerator StartGame(int seconds)
+    {
+        int count = seconds;
+
+        while (count > 0)
+        {
+
+            _tmp.SetText(count.ToString());
+            Debug.Log(_tmp);
+            yield return new WaitForSecondsRealtime(1);
+            count--;
+        }
+
+        // count down is finished...
+        _tmp.enabled = false;
+        Time.timeScale = 1;
+    }
+}
