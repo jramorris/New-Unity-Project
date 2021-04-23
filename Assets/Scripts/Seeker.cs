3998c2fbc18ab4205e02b1dac3c06a3866d17a70
James <jamesramorris@gmail.com>
2021-04-23 13:42:20 -0700

    seeker prefab

--- Assets/Scripts/Seeker.cs
+++ Assets/Scripts/Seeker.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Seeker : MonoBehaviour
 {
@@ -6,24 +7,33 @@ public class Seeker : MonoBehaviour
     [SerializeField] float _movementSpeed = 1f;
 
     Transform _playerTransform;
+    Rigidbody2D _rb;
     Vector3 relativePosition;
     Quaternion toQuaternion;
 
     void Awake()
     {
         _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        _rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         relativePosition = _playerTransform.position - transform.position;
         toQuaternion = Quaternion.FromToRotation(Vector3.up, relativePosition);
         toQuaternion = Quaternion.Euler(new Vector3(0f, 0f, toQuaternion.eulerAngles.z + 90));
         transform.rotation = Quaternion.Slerp(transform.rotation, toQuaternion, Time.deltaTime * _rotationMultiplier);
 
-        transform.position = Vector3.MoveTowards(transform.position, relativePosition, Time.deltaTime * _movementSpeed);
-        //Debug.Log($"would be pos: {transform.position + (transform.right * _movementSpeed)}");
-        //Debug.Log($"lerped pos: {Vector3.Lerp(transform.position, transform.position + (transform.right * _movementSpeed), Time.deltaTime)}");
-        //transform.position += Vector3.Lerp(transform.position, , Time.deltaTime);
+        _rb.velocity = transform.right * _movementSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Explode();
+    }
+
+    private void Explode()
+    {
+        gameObject.SetActive(false);
     }
 }
