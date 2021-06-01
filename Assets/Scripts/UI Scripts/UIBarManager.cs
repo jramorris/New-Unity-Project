f6010da63faaf4ff7fd3e538b76aaf5f7f19760d
James <jamesramorris@gmail.com>
2021-05-31 20:47:10 -0700

    fuel bar animation

--- Assets/Scripts/UI Scripts/UIBarManager.cs
+++ Assets/Scripts/UI Scripts/UIBarManager.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class UIBarManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerController _playerController;
+    private float _maxCharge;
+    private float _maxPower;
+    private Image _fuelBarImage;
+    private Material _fuelBarMaterial;
+    private float _fillAmount;
+
+    void Awake()
     {
-        
+        _playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        _maxCharge = _playerController._maxCharge;
+        _maxPower = _playerController._maxPower;
+        _fuelBarImage = GetComponent<Image>();
+        _fuelBarMaterial = _fuelBarImage.material;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateFuelBar(_playerController._currentPower);
+    }
+
+    void UpdateFuelBar(float currentPower)
+    {
+        _fillAmount = currentPower / _maxPower;
+
+        _fuelBarMaterial.SetFloat("_fuelFillAmount", _fillAmount);
+        _fuelBarImage.fillAmount = _fillAmount;
+
+        if (_fuelBarImage.fillAmount < _fillAmount)
+            _fuelBarImage.fillAmount += (Time.deltaTime * .1f * 3f);
+        else
+            _fuelBarImage.fillAmount = _fillAmount;
     }
 }
